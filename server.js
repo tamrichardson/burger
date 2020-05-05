41bae73002a7e47ddb2976410fe50b4c935dcda5
Tammy Richardson <atrich0220@gmail.com>
2020-05-05 11:12:02 -0600

    linked all of the files appropriately

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-var orm = require("./config/orm.js");
 var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
-var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// Parse request body as JSON
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
-
-// // Find all the pets ordering by the lowest price to the highest price.
-// orm.selectAndOrder("animal_name", "pets", "price");
-
-// // Find a pet in the pets table by an animal_name of Rachel.
-// orm.selectWhere("pets", "animal_name", "Rachel");
-
-// // Find the buyer with the most pets.
-// orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
