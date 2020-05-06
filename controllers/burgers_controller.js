f64a3c6947c2479c672929ed13c46519c68ea9af
Tammy Richardson <atrich0220@gmail.com>
2020-05-06 11:39:36 -0600

    added css

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,10 +17,10 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    cat.create([
-        "name", "sleepy"
+    burger.create([
+        "burger_name", "devoured"
     ], [
-        req.body.name, req.body.sleepy
+        req.body.name, req.body.devoured
     ], function (result) {
         // Send back the ID of the new quote
         res.json({ id: result.insertId });
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function (req, res) {
     console.log("condition", condition);
 
     burger.update({
-        sleepy: req.body.sleepy
+        devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
